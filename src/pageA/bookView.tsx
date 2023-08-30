6f720f953f104d060d951075983c1b74c9305936
lajeunf <50468728+lajeunf@users.noreply.github.com>
2023-08-29 19:09:07 -0700

    mobx-react
    mobx-react

--- src/pageA/bookView.tsx
+++ src/pageA/bookView.tsx
@@ -24,34 +24,51 @@ export class BookView extends Component < IBookViewProps > {
         let book = selectedId ? ProjectStore.getInstance().books.get( selectedId ) : null;
 
         if (book) {
-            return <fieldset style ={{ width: "100%", borderRadius: "8px", padding: "0px" }} >
+            return <fieldset style ={{ width: "100%", borderRadius: "8px" }} >
                 <legend style={{marginLeft:"10px"}}>Book Info</legend>
-                <div>                
-                    Title : 
-                    <InputControl value={book.title} onChange={ (evt) => {                        
+                <div style={{padding:"8px"}}>
+                    Title :
+                    <InputControl value={book.title} onChange={ (evt) => {
                             if (book) {
                                 book.title = evt.target.value;
                                 if (this.props.onChange) { this.props.onChange(evt); }
-                            }                        
+                            }
                         }}>
                     </InputControl>
                 </div>
-                <div>                
-                    Author : 
-                    <select value={ book.authorId } 
+
+                <div style={{padding:"8px"}}>
+                    Author :
+                    <select value={ book.authorId }
                         onChange={ (evt) => {
-                            if (book) { 
-                                book.authorId = evt.target.value;                                
+                            if (book) {
+                                book.authorId = evt.target.value;
                             }
                         }}
-                    >                                
+                    >
                         {
                             Array.from(ProjectStore.getInstance().authors).map( (author) => {
                                 return <option key={author[0]} value={author[0]} style={{backgroundColor: "#454545"}}>{author[1].fullName}</option>;
                             })
                         }
                     </select>
-                        
+                </div>
+
+                <div style={{padding:"8px"}}>
+                    Publisher :
+                    <select value={ book.publisherId }
+                        onChange={ (evt) => {
+                            if (book) {
+                                book.publisherId = evt.target.value;
+                            }
+                        }}
+                    >
+                        {
+                            Array.from(ProjectStore.getInstance().publishers).map( (publisher) => {
+                                return <option key={publisher[0]} value={publisher[0]} style={{backgroundColor: "#454545"}}>{publisher[1].name}</option>;
+                            })
+                        }
+                    </select>
                 </div>
             </fieldset>
         } else {
